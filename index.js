9fda128d4a7fcb84ddc404aadde3a06d080f8291
Ayush Narang <108880041+ayushnarangofficial@users.noreply.github.com>
2022-10-08 23:38:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,36 +29,43 @@ function makeSound(key) {
        switch (key) {
     case "w":
         let crash = new Audio("crash.mp3");
+        crash.currentTime= 0;
         crash.play();
         break;
 
     case "a":
         let kick = new Audio("kick-bass.mp3");
+         kick.currentTime= 0;
         kick.play();
         break;
 
     case "s":
         let snare = new Audio("snare.mp3");
+        snare.currentTime= 0;   
         snare.play();
         break;   
         
     case "d":
         let tom1 = new Audio("tom-1.mp3");
+        tom1.currentTime= 0;   
         tom1.play();
         break;        
         
     case "j":
         let tom2 = new Audio("tom-2.mp3");
+         tom2.currentTime= 0;  
         tom2.play();
         break;
         
     case "k":
         let tom3 = new Audio("tom-3.mp3");
+        tom3.currentTime= 0;   
         tom3.play();
         break; 
         
     case "l":
         let tom4 = new Audio("tom-4.mp3");
+        tom4.currentTime= 0;   
         tom4.play();
         break;     
 
